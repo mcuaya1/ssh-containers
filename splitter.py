3ea5a90500b289f7a4877d55af5517321de9f17e
Kareem S <ksheh002@ucr.edu>
2025-10-07 17:54:15 -0700

    add no auth

--- splitter.py
+++ splitter.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2003-2007  Robey Pointer <robeypointer@gmail.com>
-#
-# This file is part of paramiko.
-#
-# Paramiko is free software; you can redistribute it and/or modify it under the
-# terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation; either version 2.1 of the License, or (at your option)
-# any later version.
-#
-# Paramiko is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Paramiko; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
-
 import socket
 import sys
 import threading
@@ -58,6 +40,8 @@ class Server(paramiko.ServerInterface):
             case "auth-dict":
                 if(self.auth_dict[username] == password):
                     return paramiko.AUTH_SUCCESSFUL
+            case "no-auth":
+                return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
 
     def get_allowed_auths(self, username):
